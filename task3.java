849f1679eb33d955de5527599f1d4024fd22f3c7
Manas285 <133373276+Manas285@users.noreply.github.com>
2024-07-25 04:19:23 +0530

    Update task3.java

--- task3.java
+++ task3.java
@@ -6,26 +6,26 @@ public static void main(String[] args)
 Scanner ob=new Scanner(System.in);
 do{
 System.out.println("Enter test grade and weight in percentage :");
-double a=ob.nextDouble();
-double b=ob.nextDouble();
-double c=a*b;
+double a=ob.nextDouble(); //input obtained grade in test 
+double b=ob.nextDouble(); //input weight of test marks
+double c=a*b; //calculate weighted grade in test
 System.out.println("Enter assignment grade and weight in percentage :");
-double e=ob.nextDouble();
-double f=ob.nextDouble();
-double g=e*f;
+double e=ob.nextDouble(); //input obtained grade in assignment 
+double f=ob.nextDouble(); //input weight of assignment marks
+double g=e*f; //calculate weighted grade in assignment
 System.out.println("Enter quiz grade and weight in percentage :");
-double i=ob.nextDouble();
-double j=ob.nextDouble();
-double k=i*j;
+double i=ob.nextDouble(); //input obtained grade in quiz
+double j=ob.nextDouble(); //input weight of quiz marks
+double k=i*j; //calculate weighted grade in quiz
 System.out.println("Enter mid term exam grade and weight in percentage :");
-double m=ob.nextDouble();
-double n=ob.nextDouble();
-double o=m*n;
+double m=ob.nextDouble(); //input obtained grade in mid term exam
+double n=ob.nextDouble(); //input weight of mid term exam marks
+double o=m*n; //calculate weighted grade in mid term exam
 System.out.println("Enter final exam grade and weight in percentage :");
-double q=ob.nextDouble();
-double r=ob.nextDouble();
-double s=q*r;
-double total=(c+g+k+o+s)/(b+f+j+n+r);
+double q=ob.nextDouble(); //input obtained grade in final exam
+double r=ob.nextDouble(); //input weight of final exam marks
+double s=q*r; //calculate weighted grade in final exam
+double total=(c+g+k+o+s)/(b+f+j+n+r); //calculate final marks in percentage of all components of course
 if(total>=90.0&&total<=100.0)
 System.out.print("Final grade : A\n");
 else if(total>=75.0&&total<90.0)
@@ -37,7 +37,7 @@ System.out.print("Final grade : D\n");
 else
 System.out.print("Final grade : F\n");
 System.out.println("\nWant to continue ? press 1 for Yes or 0 for No :");
-z=ob.nextInt();
+z=ob.nextInt(); //input choice of user to continue or not
 }
 while(z!=0);
 }}
